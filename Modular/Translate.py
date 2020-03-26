85b38c24f3933287ca0b137c709af92969beb2f8
HuKeqing <keqing.hu@icloud.com>
2020-03-26 23:01:27 +0800

    refactor(Translate): adjust the file system

--- Modular/Translate.py
+++ Modular/Translate.py
@@ -1,1 +1,14 @@
 from SystemAPI import *
+
+md5 = NetWorkFunc.md5
+rand = NetWorkFunc.random
+base64 = NetWorkFunc.base64
+
+
+class BaiduTrans:
+    lan = {'zh': 'zh', 'jp': 'jp', 'en': 'en', 'kor': 'kor'}
+
+    def __init__(self, api_id, secret_key):
+        self.app_id = api_id
+        self.secret_key = secret_key
+        # self.init()
